614558d8c2b1fc2f87818837e0b7c57a22a10be5
Bhargaw Kumar Singh <bhargaw@Bhargaws-MacBook-Air.local>
2025-02-03 23:59:30 +0530

    first commit

---
+++ src/main/java/com/springboot/Deepseek/DeepseekWithSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.Deepseek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeepseekWithSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeepseekWithSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/Deepseek/DeepseekWithSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.Deepseek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeepseekWithSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
